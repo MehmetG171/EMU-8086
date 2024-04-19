71afeb4628acb5e0c96c747123139779ef6f4945
MehmetG171 <68816334+MehmetG171@users.noreply.github.com>
2024-04-19 17:46:30 +0300

    Final Commit

--- SequenceDetectors/PositionZero.asm
+++ SequenceDetectors/PositionZero.asm
@@ -3,11 +3,11 @@
 
 org 100h
 
-mov bx,10111111b ;8-bit number  
-mov ax,0         ;set ax as position
+mov bx,10111111b ; 8-bit number  
+mov ax,0         ; Set ax as position
 
-Work: inc ax     ;increment position
-      shr bx,1   ;shift bx to the right
-      jc  Work   ;finish the process if carry flag is 0.
+Work: inc ax     ; Increment position
+      shr bx,1   ; Shift bx to the right
+      jc  Work   ; Finish the process if carry flag is 0.
       
 ret
